2e2d7c8ff3a42cc88d0b2de48b1c5608f89e330c
ninja0404 <dengkang807@gmail.com>
2024-07-02 16:10:38 +0800

    a

--- apollosdk/apolloconfig.go
+++ apollosdk/apolloconfig.go
@@ -38,7 +38,7 @@ func NewApolloSDK(cfg SDKConfig) (*ApolloSDK, error) {
 		return clientConfig, nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to start Apollo client: %v", err)
+		return nil, fmt.Errorf("failed to start Apollo client: %w", err)
 	}
 
 	sdk := &ApolloSDK{
