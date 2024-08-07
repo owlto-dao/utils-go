c2c5f7be2371803fcc2d8a2b9bf8c06e04aed48d
ninja0404 <dengkang807@gmail.com>
2024-08-07 19:37:05 +0800

    add card

--- alert/larknotice.go
+++ alert/larknotice.go
@@ -1,7 +1,11 @@
 package alert
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-lark/lark"
+	"github.com/owlto-dao/utils-go/convert"
+	"strings"
 )
 
 var LarkBot *Bot
@@ -21,4 +25,25 @@ func NewLarkBot(appID, appSecret string) (*Bot, error) {
 	}
 	LarkBot = larkBot
 	return larkBot, nil
+}
+
+func (b *Bot) SendMessageCardByID(ctx context.Context, cardID string, chatIDOrOpenID string, template map[string]interface{}) error {
+	var format = `{"type":"template","data":{"template_id":"%s","template_variable":%s}}`
+	msg := lark.NewMsgBuffer(lark.MsgInteractive)
+	if strings.HasPrefix(chatIDOrOpenID, "oc_") {
+		om := msg.BindChatID(chatIDOrOpenID).Card(fmt.Sprintf(format, cardID, convert.ConvertToJsonString(template))).Build()
+		_, err := b.Bot.PostMessage(om)
+		if err != nil {
+			return err
+		}
+	} else if strings.HasPrefix(chatIDOrOpenID, "ou_") {
+		om := msg.BindOpenID(chatIDOrOpenID).Card(fmt.Sprintf(format, cardID, convert.ConvertToJsonString(template))).Build()
+		_, err := b.Bot.PostMessage(om)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("unknown chatIDOrOpenID: %s", chatIDOrOpenID)
+	}
+	return nil
 }
