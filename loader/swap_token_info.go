a090c518d20d97d0405fd6352ac953b52f29f44d
sunjian <sc_sunjian@163.com>
2024-09-04 16:31:43 +0800

    swap token manager

--- loader/swap_token_info.go
+++ loader/swap_token_info.go
@@ -2,207 +2,42 @@ package loader
 
 import (
 	"database/sql"
-	"log"
-	"strings"
 	"sync"
 
 	"github.com/owlto-dao/utils-go/alert"
 )
 
 type SwapTokenInfoManager struct {
-	chainNameTokenAddrs map[string]map[string]*TokenInfo
-	chainNameTokenNames map[string]map[string]*TokenInfo
-	allTokens           []*TokenInfo
-	db                  *sql.DB
-	alerter             alert.Alerter
-	mutex               *sync.RWMutex
+	allTokens []*TokenInfo
+	db        *sql.DB
+	alerter   alert.Alerter
+	mutex     *sync.RWMutex
 }
 
 func NewSwapTokenInfoManager(db *sql.DB, alerter alert.Alerter) *SwapTokenInfoManager {
 	return &SwapTokenInfoManager{
-		chainNameTokenAddrs: make(map[string]map[string]*TokenInfo),
-		chainNameTokenNames: make(map[string]map[string]*TokenInfo),
-		db:                  db,
-		alerter:             alerter,
-		mutex:               &sync.RWMutex{},
+		db:      db,
+		alerter: alerter,
+		mutex:   &sync.RWMutex{},
 	}
 }
 
 func (mgr *SwapTokenInfoManager) GetByChainNameTokenAddr(chainName string, tokenAddr string) (*TokenInfo, bool) {
-	mgr.mutex.RLock()
-	defer mgr.mutex.RUnlock()
-	tokenAddrs, ok := mgr.chainNameTokenAddrs[strings.ToLower(strings.TrimSpace(chainName))]
-	if ok {
-		token, ok := tokenAddrs[strings.ToLower(strings.TrimSpace(tokenAddr))]
-		return token, ok
-	}
-	return nil, false
-}
-
-func (mgr *SwapTokenInfoManager) AddTokenInfo(token TokenInfo) {
-	mgr.mutex.Lock()
-	defer mgr.mutex.Unlock()
-
-	tokenAddrs, ok := mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)]
-	if !ok {
-		tokenAddrs = make(map[string]*TokenInfo)
-		mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)] = tokenAddrs
-	}
-	tokenAddrs[strings.ToLower(token.TokenAddress)] = &token
-
-	tokenNames, ok := mgr.chainNameTokenNames[strings.ToLower(token.ChainName)]
-	if !ok {
-		tokenNames = make(map[string]*TokenInfo)
-		mgr.chainNameTokenNames[strings.ToLower(token.ChainName)] = tokenNames
-	}
-	tokenNames[strings.ToLower(token.TokenName)] = &token
-}
-
-func (mgr *SwapTokenInfoManager) AddToken(chainName string, tokenName string, tokenAddr string, decimals int32) {
-	mgr.mutex.Lock()
-	defer mgr.mutex.Unlock()
 	var token TokenInfo
-	token.ChainName = strings.TrimSpace(chainName)
-	token.TokenAddress = strings.TrimSpace(tokenAddr)
-	token.TokenName = strings.TrimSpace(tokenName)
-	token.Decimals = decimals
-
-	tokenAddrs, ok := mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)]
-	if !ok {
-		tokenAddrs = make(map[string]*TokenInfo)
-		mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)] = tokenAddrs
+	err := mgr.db.QueryRow("SELECT token_name, chain_name, token_address, decimals, icon FROM t_swap_token_info where chain_name = ? and token_address = ?", chainName, tokenAddr).
+		Scan(&token.TokenName, &token.ChainName, &token.TokenAddress, &token.Decimals, &token.Icon)
+	if err != nil {
+		return nil, false
 	}
-	tokenAddrs[strings.ToLower(token.TokenAddress)] = &token
-
-	tokenNames, ok := mgr.chainNameTokenNames[strings.ToLower(token.ChainName)]
-	if !ok {
-		tokenNames = make(map[string]*TokenInfo)
-		mgr.chainNameTokenNames[strings.ToLower(token.ChainName)] = tokenNames
-	}
-	tokenNames[strings.ToLower(token.TokenName)] = &token
+	return &token, true
 }
 
 func (mgr *SwapTokenInfoManager) GetByChainNameTokenName(chainName string, tokenName string) (*TokenInfo, bool) {
-	mgr.mutex.RLock()
-	defer mgr.mutex.RUnlock()
-	tokenNames, ok := mgr.chainNameTokenNames[strings.ToLower(strings.TrimSpace(chainName))]
-	if ok {
-		token, ok := tokenNames[strings.ToLower(strings.TrimSpace(tokenName))]
-		return token, ok
+	var token TokenInfo
+	err := mgr.db.QueryRow("SELECT token_name, chain_name, token_address, decimals, icon FROM t_swap_token_info where chain_name = ? and token_name = ?", chainName, tokenName).
+		Scan(&token.TokenName, &token.ChainName, &token.TokenAddress, &token.Decimals, &token.Icon)
+	if err != nil {
+		return nil, false
 	}
-	return nil, false
-}
-
-func (mgr *SwapTokenInfoManager) GetTokenAddresses(chainName string) []string {
-	addrs := make([]string, 0)
-	mgr.mutex.RLock()
-	tokenAddrs, ok := mgr.chainNameTokenAddrs[strings.ToLower(strings.TrimSpace(chainName))]
-	if ok {
-		for _, token := range tokenAddrs {
-			addrs = append(addrs, token.TokenAddress)
-		}
-	}
-	mgr.mutex.RUnlock()
-	return addrs
-}
-
-func (mgr *SwapTokenInfoManager) GetAllTokens() []*TokenInfo {
-	return mgr.allTokens
-}
-
-func (mgr *SwapTokenInfoManager) MergeNativeTokens(chainManager ChainInfoManager) {
-	allIDs := chainManager.GetChainInfoIds()
-
-	mgr.mutex.Lock()
-	defer mgr.mutex.Unlock()
-	for _, id := range allIDs {
-		chainInfo, ok := chainManager.GetChainInfoById(id)
-		if !ok {
-			continue
-		}
-		var token TokenInfo
-		token.ChainName = chainInfo.Name
-		token.TokenAddress = "0x0000000000000000000000000000000000000000"
-		token.TokenName = chainInfo.GasTokenName
-		token.Decimals = chainInfo.GasTokenDecimal
-
-		tokenAddrs, ok := mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)]
-		if !ok {
-			tokenAddrs = make(map[string]*TokenInfo)
-			mgr.chainNameTokenAddrs[strings.ToLower(token.ChainName)] = tokenAddrs
-		}
-		tokenNames, ok := mgr.chainNameTokenNames[strings.ToLower(token.ChainName)]
-		if !ok {
-			tokenNames = make(map[string]*TokenInfo)
-			mgr.chainNameTokenNames[strings.ToLower(token.ChainName)] = tokenNames
-		}
-		_, ok = mgr.chainNameTokenNames[strings.ToLower(token.ChainName)][strings.ToLower(token.TokenName)]
-		if !ok {
-			tokenNames[strings.ToLower(token.TokenName)] = &token
-			tokenAddrs[strings.ToLower(token.TokenAddress)] = &token
-			mgr.allTokens = append(mgr.allTokens, &token)
-		}
-
-	}
-
-}
-
-func (mgr *SwapTokenInfoManager) LoadAllToken() {
-	// Query the database to select only id and name fields
-	rows, err := mgr.db.Query("SELECT token_name, chain_name, token_address, decimals, icon FROM t_swap_token_info")
-
-	if err != nil || rows == nil {
-		mgr.alerter.AlertText("select t_swap_token_info error", err)
-		return
-	}
-
-	defer rows.Close()
-
-	chainNameTokenAddrs := make(map[string]map[string]*TokenInfo)
-	chainNameTokenNames := make(map[string]map[string]*TokenInfo)
-	allTokens := make([]*TokenInfo, 0)
-	counter := 0
-
-	// Iterate over the result set
-	for rows.Next() {
-		var token TokenInfo
-
-		if err := rows.Scan(&token.TokenName, &token.ChainName, &token.TokenAddress, &token.Decimals, &token.Icon); err != nil {
-			mgr.alerter.AlertText("scan t_swap_token_info row error", err)
-		} else {
-			token.ChainName = strings.TrimSpace(token.ChainName)
-			token.TokenAddress = strings.TrimSpace(token.TokenAddress)
-			token.TokenName = strings.TrimSpace(token.TokenName)
-
-			tokenAddrs, ok := chainNameTokenAddrs[strings.ToLower(token.ChainName)]
-			if !ok {
-				tokenAddrs = make(map[string]*TokenInfo)
-				chainNameTokenAddrs[strings.ToLower(token.ChainName)] = tokenAddrs
-			}
-			tokenAddrs[strings.ToLower(token.TokenAddress)] = &token
-
-			tokenNames, ok := chainNameTokenNames[strings.ToLower(token.ChainName)]
-			if !ok {
-				tokenNames = make(map[string]*TokenInfo)
-				chainNameTokenNames[strings.ToLower(token.ChainName)] = tokenNames
-			}
-			tokenNames[strings.ToLower(token.TokenName)] = &token
-			allTokens = append(allTokens, &token)
-			counter++
-		}
-	}
-
-	// Check for errors from iterating over rows
-	if err := rows.Err(); err != nil {
-		mgr.alerter.AlertText("get next t_swap_token_info row error", err)
-		return
-	}
-
-	mgr.mutex.Lock()
-	mgr.chainNameTokenAddrs = chainNameTokenAddrs
-	mgr.chainNameTokenNames = chainNameTokenNames
-	mgr.allTokens = allTokens
-	mgr.mutex.Unlock()
-	log.Println("load all token info: ", counter)
-
+	return &token, true
 }
