79711d1f73e57e2d73df0ec7aa156c72f094c0d6
ninja0404 <dengkang807@gmail.com>
2024-09-26 15:30:52 +0800

    update starknet

--- convert/convert.go
+++ convert/convert.go
@@ -6,11 +6,22 @@ import (
 	"time"
 )
 
-func ConvertIntToString[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value T) string {
+func IntToBool[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value T) bool {
+	return value != 0
+}
+
+func BoolToInt[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value bool) T {
+	if value {
+		return T(1)
+	}
+	return T(0)
+}
+
+func IntToString[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value T) string {
 	return strconv.FormatInt(int64(value), 10)
 }
 
-func ConvertStringToInt[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value string) T {
+func StringToInt[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value string) T {
 	result, err := strconv.ParseInt(value, 10, 64)
 	if err != nil {
 		return 0
@@ -18,7 +29,7 @@ func ConvertStringToInt[T ~int | ~int8 | ~int16 | ~int32 | ~int64](value string)
 	return T(result)
 }
 
-func ConvertStringToUint64(value string) uint64 {
+func StringToUint64(value string) uint64 {
 	result, err := strconv.ParseUint(value, 10, 64)
 	if err != nil {
 		return 0
@@ -26,7 +37,7 @@ func ConvertStringToUint64(value string) uint64 {
 	return result
 }
 
-func ConvertStringToPtrTime(value string) *time.Time {
+func StringToPtrTime(value string) *time.Time {
 	t, err := time.Parse(time.RFC3339, value)
 	if err != nil {
 		return nil
@@ -34,7 +45,7 @@ func ConvertStringToPtrTime(value string) *time.Time {
 	return &t
 }
 
-func ConvertStringToTime(value string) time.Time {
+func StringToTime(value string) time.Time {
 	t, err := time.Parse(time.RFC3339, value)
 	if err != nil {
 		return time.Time{}
@@ -42,7 +53,7 @@ func ConvertStringToTime(value string) time.Time {
 	return t
 }
 
-func ConvertStringToFloat64(value string) float64 {
+func StringToFloat64(value string) float64 {
 	result, err := strconv.ParseFloat(value, 64)
 	if err != nil {
 		return 0
