9dd7145f6d10606d9089c6fce50489e51766cd08
ninja0404 <dengkang807@gmail.com>
2024-09-29 04:22:33 +0800

    br

--- util/common.go
+++ util/common.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/shopspring/decimal"
 )
 
 func IsHexStringZero(hexString string) bool {
@@ -42,66 +43,27 @@ func GetJsonBigInt(itf interface{}) *big.Int {
 }
 
 func FromUiString(amount string, decimals int32) (*big.Int, error) {
-	// Convert the amount string to a big.Float
-	amountFloat, _, err := new(big.Float).SetPrec(236).Parse(amount, 10)
+	v, err := decimal.NewFromString(amount)
 	if err != nil {
 		return nil, err
 	}
-
-	// Scale the amount by 10^(decimals)
-	scale := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil)
-	amountScaled := new(big.Float).SetPrec(236).Mul(amountFloat, new(big.Float).SetPrec(236).SetInt(scale))
-
-	// Convert the scaled amount to a big.Int
-	amountBigInt := new(big.Int)
-	amountScaled.Int(amountBigInt)
-
-	return amountBigInt, nil
+	return v.Shift(decimals).BigInt(), nil
 }
 
 func FromUiFloat(amount float64, decimals int32) *big.Int {
-	// Convert the amount string to a big.Float
-	amountFloat := new(big.Float).SetPrec(236).SetFloat64(amount)
-
-	// Scale the amount by 10^(decimals)
-	scale := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil)
-	amountScaled := new(big.Float).SetPrec(236).Mul(amountFloat, new(big.Float).SetPrec(236).SetInt(scale))
-
-	// Convert the scaled amount to a big.Int
-	amountBigInt := new(big.Int)
-	amountScaled.Int(amountBigInt)
-
-	return amountBigInt
+	return decimal.NewFromFloat(amount).Shift(decimals).BigInt()
 }
 
 func StringToUi(amountStr string, decimals int32) (*big.Float, error) {
-	// Parse amount string to a big.Int
-	amountBigInt, success := new(big.Int).SetString(amountStr, 10)
-	if !success {
+	v, err := decimal.NewFromString(amountStr)
+	if err != nil {
 		return nil, fmt.Errorf("invalid amount string: %s", amountStr)
 	}
-
-	// Convert amount to a big.Float
-	amountFloat := new(big.Float).SetPrec(236).SetInt(amountBigInt)
-
-	// Scale down the amount by 10^(decimals)
-	scale := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil)
-	divider := new(big.Float).SetPrec(236).SetInt(scale)
-	amountFloat.Quo(amountFloat, divider)
-
-	return amountFloat, nil
+	return v.Shift(-decimals).BigFloat(), nil
 }
 
 func BigIntToUi(amount *big.Int, decimals int32) *big.Float {
-	// Convert amount to a big.Float
-	amountFloat := new(big.Float).SetPrec(236).SetInt(amount)
-
-	// Scale down the amount by 10^(decimals)
-	scale := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil)
-	divider := new(big.Float).SetPrec(236).SetInt(scale)
-	amountFloat.Quo(amountFloat, divider)
-
-	return amountFloat
+	return decimal.NewFromBigInt(amount, -decimals).BigFloat()
 }
 
 func IsEvmAddress(address string, chainID int32) bool {
