bd8a9754f9d12932aece092bdc74e114eb2362ec
gemenite <gemenite@gmail.com>
2024-12-11 18:11:46 +0800

    add token 2022 util

--- txn/solana/spl.go
+++ txn/solana/spl.go
@@ -50,6 +50,48 @@ func SplApproveBody(senderAddr string, tokenAddr string, spenderAddr string, amo
 
 }
 
+func Spl2022ApproveBody(senderAddr string, tokenAddr string, spenderAddr string, amount *big.Int, decimals int32) ([]byte, error) {
+	senderAddr = strings.TrimSpace(senderAddr)
+	tokenAddr = strings.TrimSpace(tokenAddr)
+	spenderAddr = strings.TrimSpace(spenderAddr)
+
+	senderpk, err := solana.PublicKeyFromBase58(senderAddr)
+	if err != nil {
+		return nil, err
+	}
+	mintpk, err := solana.PublicKeyFromBase58(tokenAddr)
+	if err != nil {
+		return nil, err
+	}
+	spenderpk, err := solana.PublicKeyFromBase58(spenderAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	senderAta, err := Get2022AtaFromPk(senderpk, mintpk)
+	if err != nil {
+		return nil, err
+	}
+
+	spenderAta, err := Get2022AtaFromPk(spenderpk, mintpk)
+	if err != nil {
+		return nil, err
+	}
+
+	inst := token.NewApproveCheckedInstruction(
+		amount.Uint64(),
+		uint8(decimals),
+		senderAta,
+		mintpk,
+		spenderAta,
+		senderpk,
+		[]solana.PublicKey{},
+	).Build()
+
+	return ToBody([]solana.Instruction{inst}, nil)
+
+}
+
 func SqlTransferBody(senderAddr string, tokenAddr string, receiverAddr string, amount *big.Int, decimals int32) ([]byte, error) {
 	senderAddr = strings.TrimSpace(senderAddr)
 	tokenAddr = strings.TrimSpace(tokenAddr)
@@ -74,6 +116,58 @@ func SqlTransferBody(senderAddr string, tokenAddr string, receiverAddr string, a
 	}
 
 	receiverAta, err := GetAtaFromPk(receiverpk, mintpk)
+	if err != nil {
+		return nil, err
+	}
+
+	if decimals >= 0 {
+		inst := token.NewTransferCheckedInstruction(
+			amount.Uint64(),
+			uint8(decimals),
+			senderAta,
+			mintpk,
+			receiverAta,
+			senderpk,
+			[]solana.PublicKey{},
+		).Build()
+		return ToBody([]solana.Instruction{inst}, nil)
+	} else {
+		inst := token.NewTransferInstruction(
+			amount.Uint64(),
+			senderAta,
+			receiverAta,
+			senderpk,
+			[]solana.PublicKey{},
+		).Build()
+		return ToBody([]solana.Instruction{inst}, nil)
+	}
+
+}
+
+func Sql2022TransferBody(senderAddr string, tokenAddr string, receiverAddr string, amount *big.Int, decimals int32) ([]byte, error) {
+	senderAddr = strings.TrimSpace(senderAddr)
+	tokenAddr = strings.TrimSpace(tokenAddr)
+	receiverAddr = strings.TrimSpace(receiverAddr)
+
+	senderpk, err := solana.PublicKeyFromBase58(senderAddr)
+	if err != nil {
+		return nil, err
+	}
+	mintpk, err := solana.PublicKeyFromBase58(tokenAddr)
+	if err != nil {
+		return nil, err
+	}
+	receiverpk, err := solana.PublicKeyFromBase58(receiverAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	senderAta, err := Get2022AtaFromPk(senderpk, mintpk)
+	if err != nil {
+		return nil, err
+	}
+
+	receiverAta, err := Get2022AtaFromPk(receiverpk, mintpk)
 	if err != nil {
 		return nil, err
 	}
